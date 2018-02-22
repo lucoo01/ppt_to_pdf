2a38ddf588812a8ab2ef82f1185fd7e42738d213
loen <425389019@qq.com>
2018-02-22 17:34:20 +0800

    实现将PPT转PDF的功能

--- ppt_to_pdf.py
+++ ppt_to_pdf.py
@@ -1,1 +1,29 @@
+import comtypes.client
+import os
 
+def init_powerpoint():
+    powerpoint = comtype.client.CreateObject("Powerpoint.Application")
+    powerpoint.Visible = 1
+    return powerpoint
+
+def ppt_to_pdf(powerpoint, inputFileName, outputFileName, formatType = 32):
+    if outputFileName[-3:] != 'pdf':
+        outputFileName += ".pdf"
+    phander = powerpoint.Presentations.Open(inputFileName)
+    phander.SaveAs(outputFileName, formatType) # 类型 32 是将 ppt 转成 pdf 
+    phander.Close()
+
+def cover_files_in_folder(powerpoint, folder):
+    files = os.listdir()
+    pptfiles = [f for f in files if f.endswidth(".ppt", ".pptx")]
+    
+    for pptfile in pptfiles:
+        fullpath = os.path.join(cwd, pptfile)
+        ppt_to_pdf(powerpoint, fullpath, fullpath)
+    
+
+if __name__ == "__main__":
+    powerpoint = init_powerpoint()
+    cwd = os.getcwd()
+    cover_files_in_folder(powerpoint, cwd)
+    powerpoint.Quit()
